78085cc2e42044b5380a6e5b0e35665acbb986fb
alexselenga <alexselenga@yandex.ru>
2021-02-21 15:09:10 +0400

    Init Together

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -17,10 +17,9 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@app/assets';
     public $css = [
-        'css/site.css',
+        'site.css',
     ];
     public $js = [
     ];
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    outputDir: 'web',
+    filenameHashing: false, //?
+    indexPath: '../views/layouts/main-vue.php',
+}
